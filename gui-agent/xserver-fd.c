20778b2b0c466b3c26f103ff739f18ff60fab2c2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-01 04:50:14 +0100

    code style: fix indentation, no functional change

--- gui-agent/xserver-fd.c
+++ gui-agent/xserver-fd.c
@@ -29,39 +29,39 @@
 #define SOCKET_ADDRESS  "/var/run/xf86-qubes-socket"
 void wait_for_unix_socket(int *fd)
 {
-	struct sockaddr_un sockname, peer;
-	int s;
-	unsigned int addrlen;
-	int prev_umask;
-	
-	unlink(SOCKET_ADDRESS);
-	s = socket(AF_UNIX, SOCK_STREAM, 0);
-	memset(&sockname, 0, sizeof(sockname));
-	sockname.sun_family = AF_UNIX;
-	memcpy(sockname.sun_path, SOCKET_ADDRESS, strlen(SOCKET_ADDRESS));
+    struct sockaddr_un sockname, peer;
+    int s;
+    unsigned int addrlen;
+    int prev_umask;
 
-	prev_umask=umask(077);
-	if (bind(s, (struct sockaddr *) &sockname, sizeof(sockname)) == -1) {
-		printf("bind() failed\n");
-		close(s);
-		exit(1);
-	}
-	umask(prev_umask);
-//	chmod(sockname.sun_path, 0666);
-	if (listen(s, 5) == -1) {
-		perror("listen() failed\n");
-		close(s);
-		exit(1);
-	}
+    unlink(SOCKET_ADDRESS);
+    s = socket(AF_UNIX, SOCK_STREAM, 0);
+    memset(&sockname, 0, sizeof(sockname));
+    sockname.sun_family = AF_UNIX;
+    memcpy(sockname.sun_path, SOCKET_ADDRESS, strlen(SOCKET_ADDRESS));
 
-	addrlen = sizeof(peer);
-	fprintf (stderr, "Waiting on %s socket...\n", SOCKET_ADDRESS);
-	*fd = accept(s, (struct sockaddr *) &peer, &addrlen);
-	if (*fd == 1) {
-		perror("unix accept");
-		exit(1);
-	}
-	fprintf (stderr, "Ok, somebody connected.\n");
-	close(s);
-	//	sleep(2);		//give xserver time to boot up
+    prev_umask=umask(077);
+    if (bind(s, (struct sockaddr *) &sockname, sizeof(sockname)) == -1) {
+        printf("bind() failed\n");
+        close(s);
+        exit(1);
+    }
+    umask(prev_umask);
+    //	chmod(sockname.sun_path, 0666);
+    if (listen(s, 5) == -1) {
+        perror("listen() failed\n");
+        close(s);
+        exit(1);
+    }
+
+    addrlen = sizeof(peer);
+    fprintf (stderr, "Waiting on %s socket...\n", SOCKET_ADDRESS);
+    *fd = accept(s, (struct sockaddr *) &peer, &addrlen);
+    if (*fd == 1) {
+        perror("unix accept");
+        exit(1);
+    }
+    fprintf (stderr, "Ok, somebody connected.\n");
+    close(s);
+    //	sleep(2);		//give xserver time to boot up
 }
