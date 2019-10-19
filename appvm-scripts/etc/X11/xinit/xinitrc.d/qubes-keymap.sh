56262fe58bbfddf87e3ee56b4c9332981aa902ba
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-10-19 22:00:53 +0200

    qubes-keymap: handle input of keyboard layout

--- appvm-scripts/etc/X11/xinit/xinitrc.d/qubes-keymap.sh
+++ appvm-scripts/etc/X11/xinit/xinitrc.d/qubes-keymap.sh
@@ -2,19 +2,18 @@
 
 # This file may be also executed by qubes-change-keyboard-layout
 
-QUBES_KEYMAP="`/usr/bin/qubesdb-read /qubes-keyboard`"
-QUBES_KEYMAP="`/bin/echo -e $QUBES_KEYMAP`"
-QUBES_USER_KEYMAP=`cat $HOME/.config/qubes-keyboard-layout.rc 2> /dev/null`
+QUBES_KEYMAP="$(/usr/bin/qubesdb-read /keyboard-layout)"
+QUBES_USER_KEYMAP="$(cat "$HOME/.config/qubes-keyboard-layout.rc" 2> /dev/null)"
 
 if [ -n "$QUBES_KEYMAP" ]; then
-    echo "$QUBES_KEYMAP" | xkbcomp - $DISPLAY
+    setxkbmap -layout "$QUBES_KEYMAP"
 fi
 
 if [ -n "$QUBES_USER_KEYMAP" ]; then
-    QUBES_USER_KEYMAP_LAYOUT=`echo $QUBES_USER_KEYMAP+ | cut -f 1 -d +`
-    QUBES_USER_KEYMAP_VARIANT=`echo $QUBES_USER_KEYMAP+ | cut -f 2 -d +`
+    QUBES_USER_KEYMAP_LAYOUT="$(echo "$QUBES_USER_KEYMAP"+ | cut -f 1 -d +)"
+    QUBES_USER_KEYMAP_VARIANT="$(echo "$QUBES_USER_KEYMAP"+ | cut -f 2 -d +)"
     if [ -n "$QUBES_USER_KEYMAP_VARIANT" ]; then
         QUBES_USER_KEYMAP_VARIANT="-variant $QUBES_USER_KEYMAP_VARIANT"
     fi
-    setxkbmap $QUBES_USER_KEYMAP_LAYOUT $QUBES_USER_KEYMAP_VARIANT
+    setxkbmap "$QUBES_USER_KEYMAP_LAYOUT" "$QUBES_USER_KEYMAP_VARIANT"
 fi
