2499ddfc4ff1dd4e2c77c4b50423435635308861
ejose19 <8742215+ejose19@users.noreply.github.com>
2022-06-06 21:53:03 -0300

    adjust pulsecore download script to use upstream repo to obtain headers

--- get-latest-pulsecore.sh
+++ get-latest-pulsecore.sh
@@ -5,7 +5,6 @@ set -ex
 exit_updater() {
     local exit_code=$?
     rm -rf "$TMPDIR"
-    rm -f "$LOCALDIR/${SRC_FILE:?}"
     if [ ${exit_code} -ge 1 ]; then
         echo "-> An error occurred while fetching latest pulsecore headers. Manual update is required"
     fi
@@ -21,28 +20,35 @@ if [ ! -d "$BUILDERDIR" ]; then
     exit 1
 fi
 
-if [ -z "$LATEST_FEDORA_RELEASE" ]; then
-    LATEST_FEDORA_RELEASE="$(git ls-remote --heads https://src.fedoraproject.org/rpms/fedora-release | grep -Po "refs/heads/f[0-9][1-9]*" | sed 's#refs/heads/f##g' | sort -g | tail -1)"
-fi
-LATEST_FEDORA_VERREL="$(dnf -q repoquery pulseaudio --disablerepo=* --enablerepo=fedora --enablerepo=updates --releasever="$LATEST_FEDORA_RELEASE" | grep -Po "[0-9][1-9]*\.*[0-9]*\.*[0-9]*-[0-9]*" | sort -V | tail -1)"
+REPO_URL=https://gitlab.freedesktop.org/pulseaudio/pulseaudio.git
 
-LATEST_FEDORA_VERSION="$(echo "$LATEST_FEDORA_VERREL" | cut -d'-' -f1)"
+LATEST_REPO_VERSION="$(git ls-remote --exit-code --refs --tags --sort="v:refname" "$REPO_URL" '*.*' | tail -n1 | cut -d/ -f3 | sed 's/^v//')"
 LATEST_QUBES_VERSION="$(find "$LOCALDIR/pulse" -type d -name "pulsecore-*" | sed "s|$LOCALDIR/pulse/pulsecore-||" | sort -g | tail -1)"
-
-SRC_RPM="pulseaudio-${LATEST_FEDORA_VERREL}.fc${LATEST_FEDORA_RELEASE}.src.rpm"
-SRC_FILE="pulseaudio-${LATEST_FEDORA_VERSION}.tar.xz"
 
 trap 'exit_updater' 0 1 2 3 6 15
 
-if [ "${LATEST_QUBES_VERSION}" != "${LATEST_FEDORA_VERSION}" ] && [ ! -e "$LOCALDIR/pulse/pulsecore-${LATEST_FEDORA_VERSION}" ]; then
-    "$BUILDERDIR/scripts/get_sources_from_srpm" "$LATEST_FEDORA_RELEASE" pulseaudio "$SRC_RPM" "$SRC_FILE"
+if [ "${LATEST_QUBES_VERSION}" != "${LATEST_REPO_VERSION}" ] && [ ! -e "$LOCALDIR/pulse/pulsecore-${LATEST_REPO_VERSION}" ]; then
+    cd "$TMPDIR"
+
+    git clone --depth 1 --branch "v$LATEST_REPO_VERSION" "$REPO_URL" .
+
+    # Import keys of repo taggers
+    for key in \
+        52DFA7B8BAC74687C8A88EF48165E3D1987E2132 \
+        B61E1D411D57BD16F11536162477064CE8B9F3BD; do
+        gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ||
+            gpg --batch --keyserver keys.openpgp.org --recv-keys "$key" ||
+            gpg --batch --keyserver pgp.mit.edu --recv-keys "$key" ||
+            gpg --batch --keyserver keyserver.pgp.com --recv-keys "$key" ||
+            gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"
+    done
+
+    # Verify integrity
+    git tag -v "$(git describe)"
 
     # remove unwanted files
-    cd "$TMPDIR"
-    tar -xf "$LOCALDIR/$SRC_FILE"
-    find "pulseaudio-$LATEST_FEDORA_VERSION/src/pulsecore" -type f ! -regex '.*\.h$' -exec rm -f {} \;
-    rm -f "pulseaudio-$LATEST_FEDORA_VERSION/src/Makefile"
+    find "src/pulsecore" -type f ! -regex '.*\.h$' -exec rm -f {} \;
 
     # copy to qubes-gui-agent
-    cp -r "pulseaudio-$LATEST_FEDORA_VERSION/src/pulsecore" "$LOCALDIR/pulse/pulsecore-$LATEST_FEDORA_VERSION"
+    cp -r "src/pulsecore" "$LOCALDIR/pulse/pulsecore-$LATEST_REPO_VERSION"
 fi
