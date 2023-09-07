2b1464974f1c10ce99400636043bf83a51dfbacd
Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
2023-09-07 17:02:18 +0200

    Update for Fedora 39

--- xf86-input-mfndev/autogen.sh
+++ xf86-input-mfndev/autogen.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
+set -x
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
 cd $srcdir
 
+autoupdate -v -f || exit 1
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
